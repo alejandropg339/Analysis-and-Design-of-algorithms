f472cfaf273cd98af42e596b47674eb00004bf90
alejandropg339 <57865025+alejandropg339@users.noreply.github.com>
2020-04-03 07:51:12 -0500

    Add files via upload

---
+++ taller2/ACualitativo.py
@@ -0,0 +1,45 @@
+print("Ingresa la opcion de la receta que deseas conocer para tu desayuno: ",
+      "\n","1. Huevo frito",
+      "\n","2. Chocolate",
+      "\n","3.Pan tostado con mermelada")
+
+opcion=input()
+opcion=int(opcion)
+
+if opcion==1:
+     print("Receta del huevo frito:",
+            "\n", "1. Alista un sarten pequeño, un huevo y mantequilla",
+            "\n", "2. Enciende la estufa",
+            "\n", "3. coloca el sarten en la estufa",
+            "\n", "4. corta un trozo de mantequilla y viertelo en el sarten",
+            "\n", "5. Esparcir la mantequilla por toda la superficie del sarten",
+            "\n", "6. Quebrar el huevo y vertirlo en el sarten",
+            "\n", "7. Tapar el sarten",
+            "\n", "8. Esperar a que el huevo se cocine",
+            "\n", "9. Retirar cuidadosamente el huevo del sarten",
+            "\n","Apagar la estufa"
+            "\n", "fin")
+
+if opcion==2:
+    print("Receta del chocolate:",
+          "\n","1. Alista un olleta para chcolate, un molinillo, leche y pastillas de chcolate al gusto"
+          "\n","2. Encieda la estufa"
+          "\n","3. Coloque la olleta en la estufa"
+          "\n","4. Vierta la leche en la olleta"
+          "\n","5. Agregue pastillas de chocolate al gusto",
+          "\n","6. Revoler con el molinllo hasata que hierva el chocolate"
+          "\n","7. Retirar la olleta de la estufa"
+          "\n","8. Apagara la estufa "
+          "\n","Fin")
+
+if opcion==3:
+    print("Receta pan tostado con mermelada:",
+          "\n","1. Alistar el pan tajado, la mermelada y un cuchillo",
+          "\n","2. Prender la tostadora",
+          "\n","3. Colocar las tajadas de pan en la tostadora",
+          "\n","4. Esperar a que el pan tostado este lito",
+          "\n","5. Retirar el pan tostado de la tostadora",
+          "\n","6. Destapar la mermelada",
+          "\n","7. Con el cuchillo aplicar la mermelada sobre el pan tostado",
+          "\n","Fin")
+
